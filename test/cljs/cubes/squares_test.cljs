622c14636d2c51cafc918875b2cffda448585499
Sebastian Bensusan <sbensu@gmail.com>
2016-01-27 20:27:06 +0100

    Extract logic tests to ns

---
+++ test/cljs/cubes/squares_test.cljs
@@ -0,0 +1,15 @@
+(ns cubes.squares-test
+  (:require-macros [test.helper :as helper])
+  (:require [cljs.test :refer-macros [deftest testing is use-fixtures]]
+            [cljs.reader :as reader]
+            [cubes.squares :as sq]))
+
+;; ======================================================================
+;; Logic
+
+(def db (reader/read-string (helper/load-edn "test/resources/sample-db.edn")))
+
+(deftest plan-moves
+  (let [goal [11 9]
+        plan [{:type :move, :move 11, :to 9}]]
+    (is (= plan (sq/plan-moves goal db)))))
